548ca4138fbf1e29e839ce8acb5b19147cd0e5c9
Emilio Flórez Carrascal <65036537+EmFloCar@users.noreply.github.com>
2021-11-14 01:51:55 -0500

    Update verificar.js

--- middlewares/verificar.js
+++ middlewares/verificar.js
@@ -2,7 +2,7 @@ const verificar = (req, res, next) => {
     if(req.session.isAuth){
         next()
     }else{
-      res.status(401).send('No estas autorizado')
+      res.send('No estas autorizado')
     }
   }
 
